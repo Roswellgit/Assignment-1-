7748685f27c45d28a4bc8aebdb303e28dbb6ce57
win10 <win10@DESKTOP-ORNPSN0>
2024-04-04 13:34:18 +0800

    Add project files.

---
+++ Assignment1/Calbum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cocktaillist
+{
+    public class Calbum
+    {
+        public string cname;
+        public string prep;
+        public string mainingredients;
+        public string flavor;
+        public string type1;
+        public string type2;
+    }
+}
